392f1b70b809f98cd82508fc4cb1f8dd4646ba6a
Brendan Bozorgmir <bbozorgmir@college.harvard.edu>
2014-10-07 18:39:26 -0400

    Fixed some author issues up

--- blogparser.py
+++ blogparser.py
@@ -3,51 +3,69 @@ import urllib2, csv
 import unicodecsv
 import re
 
-arr = []
+# creating a set object to store urls
 urlset = set()
+# opening a saved version of the homepage that was scrolled down all the way so it has all links
 soup = BeautifulSoup(open("/Users/brendan/Projects/advocateblogscraper/Notes from 21 South Street   The Harvard Advocate Blog.html"))
+# extracting all links
 for link in soup.findAll('a'):
     post = link.get('href')
+    # if in form of blog post
     if "http://theadvocateblog.net/2" in post:
+        #stripping comments form url link
         post = re.sub("#(.*)", "", post)
         urlset.add(post)
 
+metadata = []  
 
-metadata = []
-  
-       
+#iterating through urlset       
 for url in urlset:
+    #opening blog post
     soup = BeautifulSoup(urllib2.urlopen(url).read())
+    # extracting title
     title =  soup.find("h1", class_ = "entry-title").text
+    # extracting categories
     categories = soup.find("footer", class_ = "entry-meta").findAll(rel = "category tag")
     for x in xrange(0,len(categories)):
         categories[x] = categories[x].text
+    # extracting image urls
     image_array = []
     for image in soup.findAll('img'):
         if "gravatar" not in str(image):
             image_array.append(image['src'])
+    # extracting audio urls
     audio_array = []
     for audio in soup.findAll(type = 'audio/mpeg'):
-        audio_array.append(audio['src'])
-    author = ""
-    block = ""
-    body = soup.find("div", class_ = "entry-content").findAll(['p','li','ul'])
-    author_tag = 0
+        audio_array.append(audio['src'])  
+    # extracting date  
     date = soup.find("div", class_="entry-meta").find(class_ = "entry-date").text
+    # getting body of text
+    body = soup.find("div", class_ = "entry-content").findAll(['p','li','ul','div'])
+    author = []
+    block = ""
+    end_tag = 0
+    text_block = ""
     for x in body:
-        if (len(x.findAll(['p','li','ul'])) > 1):
-            1 + 1
-        elif ("<p><em>By" in unicode(x) or "<p><em>-" in unicode(x)):
-            author = x.text.strip("By").strip("-")
-            author_tag = 1
-        elif ".jpg" not in unicode(x) and author_tag == 0:
-            block = block + unicode(x)
-    metadata.append([title, categories, author, block, image_array, audio_array, date,url])
+        if x:
+            if (len(x.findAll(['p','li','ul','div'])) > 1):
+                1+1
+            # stop reading if it gets to this point
+            elif unicode("""<div class="wpcnt">""") in unicode(x):
+                end_tag = 1
+            elif unicode("""li class="share-twitter">""") in unicode(x):
+                end_tag = 1
+            # adding author
+            elif "<p><em>By" in unicode(x) or "<p><em>-" in unicode(x) or ("<p><em>Posted by" in unicode(x)) or ("<p>By" in unicode(x) and re.search("'1(.*</p>)",unicode(x)) is not None):
+                author.append(x.text.strip("By").strip("-").strip("Posted by"))
+            # if not at end, add to text
+            elif end_tag == 0:
+                text_block += " " + x.text
+                block += unicode(x)
+    # adding all data to metadata
+    metadata.append([title, categories, author, block, text_block, image_array, audio_array, date,url])
 
-
-            
+# writing metadata to csv
 myfile = open("blogmetadata.csv", 'wb')
 wr = unicodecsv.writer(myfile, quoting=csv.QUOTE_ALL)
 for line in metadata:
     wr.writerow(line)
-    
